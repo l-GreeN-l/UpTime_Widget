05f84c565d68fe865567bc3c9f0e5263662c4199
Z_green_Z <valdisvan@mail.ru>
2022-02-07 15:40:15 +0300

    Update Tray.java

--- UpTime/src/uptime/Tray.java
+++ UpTime/src/uptime/Tray.java
@@ -5,12 +5,7 @@
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
-
-
-
-
 package uptime;
-
 import java.awt.AWTException;
 import java.awt.Image;
 import java.awt.MenuItem;
@@ -24,95 +19,56 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import javax.swing.JOptionPane;
-
-
-
 import static uptime.File.Save;
 import static uptime.UpTime.frame;
 import static uptime.UpTime.initFrame;
 
 
-/**
- *
- * @author v.slobodchikov
- */
 public class Tray {
     
-    
    public Tray(){ 
-  
-       
-       
      // Проверка поддержики трея в ОС  
     if(!SystemTray.isSupported()){
         System.out.println("System tray is not supported !!! ");
         return ;
     }
 //______________________________________________________________________________    
-   
-    
-    
-    
+
     SystemTray systemTray = SystemTray.getSystemTray(); 
     Image image = Toolkit.getDefaultToolkit().getImage("UpT.gif");
 
-    
-    
-    
     //popupmenu
     PopupMenu trayPopupMenu = new PopupMenu();
-
+       
     //1t menuitem for popupmenu
     MenuItem action = new MenuItem("Открыть меню");
     
-    
-    
-    
-    
     action.addActionListener(new ActionListener() {
+        
         
         @Override
         public void actionPerformed(ActionEvent e) {
-            
-            
             initFrame();
-            
-            
         }
     });    
 //______________________________________________________________________________    
     
-    
-    
-    
     trayPopupMenu.add(action);
 
-    
-    
-    
     //Кнопка выход инит+ листенер
     MenuItem close = new MenuItem("Выход");
     close.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
-            
-            
+                     
             System.out.println("Exit.............");
             
             Save();
             System.exit(0);   
-            
-            
-            
+         
         }
     });
 //______________________________________________________________________________    
-    
-    
-  
-
- 
     
 
    Runtime.getRuntime().addShutdownHook(new Thread()
@@ -123,15 +79,7 @@ public class Tray {
         Save();
     }
 });
-    
-    
-    
-    
- 
 
-
-
-    
     trayPopupMenu.add(close);
 
     //setting tray icon
@@ -145,11 +93,7 @@ public class Tray {
         awtException.printStackTrace();
     }
     System.out.println("end of main");
-
-
-        
-    
-    
+  
    } 
 //------------------------------------------------------------------------------
     
